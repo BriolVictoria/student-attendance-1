63b753c530bf3b71524ff622f07192dbcbe53523
Dominique Vilain <dominiquevilain@users.noreply.github.com>
2026-04-15 20:43:06 +0200

    feat: prepare app for translation of strings and start with labels

---
+++ lang/en/form-labels.php
@@ -0,0 +1,8 @@
+<?php
+return [
+    'first_name' => 'prénom',
+    'last_name' => 'nom de famille',
+    'email' => 'adresse email',
+    'matricule' => 'matricule',
+    'birth_date' => 'date de naissance',
+];
