be16aee9d581cf96e38a92231d30088fc366a96c
Dominique Vilain <dominiquevilain@users.noreply.github.com>
2026-04-16 16:51:18 +0200

    add layout component

---
+++ resources/views/components/layout.blade.php
@@ -0,0 +1,22 @@
+@props([
+    'title',
+])
+
+    <!doctype html>
+<html lang="{{ app()->getLocale() }}">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="stylesheet" href="{{ asset('build/assets/css/app.css') }}">
+        <title>{{ $title }} | {{ config('app.name') }}</title>
+    </head>
+    <body class="layout">
+    <main class="page-main">
+        {{ $slot }}
+    </main>
+
+
+    <x-main-navigation/>
+    <x-main-footer/>
+    </body>
+</html>
---
+++ resources/views/components/main-footer.blade.php
@@ -0,0 +1,6 @@
+<footer {{ $attributes->class(['page-footer']) }}>
+    <p>
+        <time datetime="2025">2025</time>
+        - <abbr title="Système de gestion de contenus">SGC</abbr>
+    </p>
+</footer>
