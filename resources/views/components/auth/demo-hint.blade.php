2a224d9ce7ee5b63b30e5517fbc2db12098211c3
Dominique Vilain <dominiquevilain@users.noreply.github.com>
2026-05-26 23:09:01 +0200

    Fix demo hint

--- resources/views/components/auth/demo-hint.blade.php
+++ resources/views/components/auth/demo-hint.blade.php
@@ -1,8 +1,9 @@
 <div class="card card--info card--compact login-box__hint" style="margin-top: var(--space-5);">
     <p class="login-box__hint-title">Identifiants de démonstration :</p>
-    <p class="login-box__hint-text">
-        Email : prof@ecole.fr<br>
-        Mot de passe : demo123
-    </p>
+    <dl class="login-box__hint-text">
+        <dt>Email</dt>
+        <dd>d@d.com</dd>
+        <dt>Mot de passe</dt>
+        <dd>password</dd>
+    </dl>
 </div>
-
